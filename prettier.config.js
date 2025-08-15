c79cbdaa12928521e46be51301e6b74a82e461a1
yggai <pygosuperman@outlook.com>
2025-08-15 17:04:51 +0800

    项目结构优化

--- prettier.config.js
+++ prettier.config.js
@@ -5,25 +5,5 @@ export default {
   printWidth: 100,
   tabWidth: 2,
   useTabs: false,
-  quoteProps: 'as-needed',
-  bracketSpacing: true,
-  bracketSameLine: false,
-  arrowParens: 'avoid',
   endOfLine: 'lf',
-  embeddedLanguageFormatting: 'auto',
-  overrides: [
-    {
-      files: '*.json',
-      options: {
-        printWidth: 80
-      }
-    },
-    {
-      files: '*.md',
-      options: {
-        printWidth: 80,
-        proseWrap: 'always'
-      }
-    }
-  ]
 };
